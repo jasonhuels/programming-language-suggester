be00bdff4bc4d480886bd5e29186035346a49790
Jason Huels <jasonhuels@yahoo.com>
2019-08-16 10:56:22 -0700

    change style of page and change border color to user selected favorite color

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 $(function() {
   $(".questionaire").submit(function(event) {
-    // var response1 = $("input:radio[name=interest]:checked").val();
-    // var response2 = $("input:radio[name=types]:checked").val();
-    // var response3 = $("input:radio[name=code]:checked").val();
-    // var response4 = $("#punctuation").val();
-    var favoriteColor = $("#color").val();
-    var js = 0;
-    var python = 0;
-    var cSharp = 0;
     var responses = [];
-
-    $(".results").hide();
-
     responses[0] = $("input:radio[name=interest]:checked").val();
     responses[1] = $("input:radio[name=types]:checked").val();
     responses[2] = $("input:radio[name=code]:checked").val();
     responses[3] = $("#punctuation").val();
+
+    var favoriteColor = $("#color").val();
+    var js = 0;
+    var python = 0;
+    var cSharp = 0;
+
+    $(".results").hide();
+    $("#plural").hide();
 
     for(let i=0; i<responses.length; i++) {
       if(responses[i].includes("js")) {
@@ -30,27 +27,31 @@ $(function() {
     }
 
     if(python > js && python > cSharp) {
-      $(".results").text("Python");
+      $(".results #results").text("Python");
       $(".results").show();
     } else if(js > python && js > cSharp) {
-      $(".results").text("JavaScript");
+      $(".results #results").text("JavaScript");
       $(".results").show();
     } else if(cSharp > python && cSharp > js){
-      $(".results").text("C#");
+      $(".results #results").text("C#");
       $(".results").show();
     } else {
+      $(".results #plural").show();
+      $(".results #results").text("");
       if(python === Math.max(python, js, cSharp)) {
-        $(".results").append("Python");
+        $(".results #results").append("Python");
       }
       if(js === Math.max(python, js, cSharp)) {
-        $(".results").append("JavaScript");
+        $(".results #results").append("JavaScript");
       }
       if(cSharp === Math.max(python, js, cSharp)) {
-        $(".results").append("C#");
+        $(".results #results").append("C#");
       }
       $(".results").show();
     }
-    console.log(favoriteColor);
+
+    $("body").css("border-color", favoriteColor);
+    //console.log($("html").attr("border-color"));
 
     event.preventDefault();
   });
